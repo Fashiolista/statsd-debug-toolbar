a27f70922e6254b486e3f8e8c13a1bc1f97062c5
Mike Ryan <mike@fadedink.co.uk>
2011-12-14 08:22:26 -0800

    trying to get pip/setuptools working nicely

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
+from setuptools import setup
 
 setup(name='django-statsd-toolbar',
-      version='0.1',
+      version='0.1.0',
       description='statsd debug panel for djang-debug-toolbar',
       author='Mike Ryan',
       author_email='mike@fadedink.co.uk',
