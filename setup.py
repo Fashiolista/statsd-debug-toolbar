c81cb91b4c0670cd5620792b5486bdc3ef073b7b
Mike Ryan <mike@fadedink.co.uk>
2011-12-15 01:07:21 -0800

    Fixed setup.py.

--- setup.py
+++ setup.py
@@ -4,11 +4,12 @@
 from setuptools import setup
 
 setup(name='django-statsd-toolbar',
-      version='0.1.0',
+      version='0.1.1',
       description='statsd debug panel for djang-debug-toolbar',
       author='Mike Ryan',
       author_email='mike@fadedink.co.uk',
       url='https://github.com/mikery/statsd-debug-toolbar',
       packages=['statsd_toolbar'],
-      requires=['django (>=1.2)']
+      package_data = {'statsd_toolbar': ['templates/panels/*',
+                                         'panels/*']}
 )
